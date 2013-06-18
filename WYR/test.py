c9baaa82ba08df369dbe5d89d9b1d16eb3344440
PoohBear <smithers_mr@yahoo.com>
2013-06-18 14:46:42 -0600

    removed a couple of things

--- WYR/test.py
+++
@@ -1,9 +0,0 @@
-from supybot.test import *
-
-class WYRTestCase(PluginTestCase):
-    plugins = ('WYR',)
-
-    def testWYR(self):
-        assert(True)
-
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
